dc5497db69af97d48c622aa1bb338fe8161b41ab
sanjay-budige <Sanjay.budige@gmail.com>
2024-09-03 22:35:40 +0530

    initial files

---
+++ src/main/java/com/chatbot_justice/chatbot_justice/ChatbotJusticeApplication.java
@@ -0,0 +1,13 @@
+package com.chatbot_justice.chatbot_justice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatbotJusticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatbotJusticeApplication.class, args);
+	}
+
+}
